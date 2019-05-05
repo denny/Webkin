8bfabb1f8db408324f8d145aaf0ac9782343572f
Denny de la Haye <2018@denny.me>
2019-05-05 23:40:04 +0100

    Consistent use of whitespace

--- spec/story_spec.rb
+++ spec/story_spec.rb
@@ -34,7 +34,7 @@ describe Story do
     context 'If a supported but non-existent URL is set' do
       it 'it throws an error' do
         s = Story.new
-        VCR.use_cassette('fanfiction.net_NOPE') do
+        VCR.use_cassette( 'fanfiction.net_NOPE' ) do
           expect { s.url = 'https://www.fanfiction.net/NOPE' }.to raise_error(
             ArgumentError, %r{Story not found.}
           )
@@ -47,7 +47,7 @@ describe Story do
     context 'If a supported URL is set' do
       it 'it loads the plugin' do
         story = Story.new
-        VCR.use_cassette('fanfiction.net_Cobalts-Edge') do
+        VCR.use_cassette( 'fanfiction.net_Cobalts-Edge' ) do
           story.url = 'https://www.fanfiction.net/s/12734411/1/Cobalt-s-Edge'
           expect( story.respond_to?( :fetch ) ).to eql true
         end
@@ -70,7 +70,8 @@ describe Story do
     context 'If a supported URL is set' do
       it 'it fetches the page' do
         story = Story.new
-        VCR.use_cassette('fanfiction.net_Cobalts-Edge', record: :new_episode) do
+        VCR.use_cassette( 'fanfiction.net_Cobalts-Edge',
+                          record: :new_episodes ) do
           story.url = 'https://www.fanfiction.net/s/12734411/1/Cobalt-s-Edge'
           story.fetch
           expect( story.html ).to match %r{<h1>Cobalt's Edge</h1>}m
@@ -83,7 +84,7 @@ describe Story do
     context 'If a multi-page story is requested' do
       it 'it fetches all of the pages' do
         story = Story.new
-        VCR.use_cassette('fanfiction.net_Cobalts-Edge') do
+        VCR.use_cassette( 'fanfiction.net_Cobalts-Edge' ) do
           story.url = 'https://www.fanfiction.net/s/12734411/1/Cobalt-s-Edge'
           story.fetch
           expect( story.html ).to match %r{Her name's 'Cobalt's Edge'.}m
